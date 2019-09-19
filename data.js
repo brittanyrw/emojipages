aca78aa2aaf34c9beabea367654a8e7215d0b91b
Brittany Walker <breneewalker@gmail.com>
2019-09-19 14:43:11 -0400

    update mobile styling

--- data.js
+++ data.js
@@ -48,6 +48,55 @@ const emojiItems = [
         link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
         genres: ["fantasy", "young-adult", "fiction", "magic"],
         year: 1998
+    }, {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: ["J.K. Rowling"],
+        emojiImgs: "🧙‍♂️🐍📓👩⚡️",
+        link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
+        genres: ["fantasy", "young-adult", "fiction", "magic"],
+        year: 1998
+    }, {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: ["J.K. Rowling"],
+        emojiImgs: "🧙‍♂️🐍📓👩⚡️",
+        link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
+        genres: ["fantasy", "young-adult", "fiction", "magic"],
+        year: 1998
+    }, {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: ["J.K. Rowling"],
+        emojiImgs: "🧙‍♂️🐍📓👩⚡️",
+        link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
+        genres: ["fantasy", "young-adult", "fiction", "magic"],
+        year: 1998
+    }, {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: ["J.K. Rowling"],
+        emojiImgs: "🧙‍♂️🐍📓👩⚡️",
+        link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
+        genres: ["fantasy", "young-adult", "fiction", "magic"],
+        year: 1998
+    }, {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: ["J.K. Rowling"],
+        emojiImgs: "🧙‍♂️🐍📓👩⚡️",
+        link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
+        genres: ["fantasy", "young-adult", "fiction", "magic"],
+        year: 1998
+    }, {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: ["J.K. Rowling"],
+        emojiImgs: "🧙‍♂️🐍📓👩⚡️",
+        link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
+        genres: ["fantasy", "young-adult", "fiction", "magic"],
+        year: 1998
+    }, {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: ["J.K. Rowling"],
+        emojiImgs: "🧙‍♂️🐍📓👩⚡️",
+        link: "https://www.jkrowling.com/book/harry-potter-chamber-secrets/",
+        genres: ["fantasy", "young-adult", "fiction", "magic"],
+        year: 1998
     }
     
 ]
